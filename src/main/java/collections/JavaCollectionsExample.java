7f6d79f58d8d19b9ce3b61c015a9d446a93945e5
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-04 11:13:10 +0200

    ``

--- src/main/java/collections/JavaCollectionsExample.java
+++ src/main/java/collections/JavaCollectionsExample.java
@@ -6,31 +6,30 @@ import java.util.List;
 
 public class JavaCollectionsExample {
     public void arrayOperations() {
-        try {
-            //utworzenie tablicy
+        try {       // między nawiasami wprowadzamy kod, który może generować wyjątek
+            // utworzenie tablicy 10-cio elementowej
             String[] names = new String[10];
-            int[] numbers = {1, 2, 3, 4, 5};
-            System.out.println("Tablica nazaw");
-            Arrays.stream(names) //zamiana tablicy na stream
-                    .forEach(s -> System.out.println(s)); // dla każdego elementu wykonaj predykat (CTRL + Soace)
-//        for(String s : names){
-//            System.out.println(s);
-//        }
-            System.out.println("modyfikacja wartości na indeksie");
-            names[names.length - 1] = "Anna";
-            Arrays.stream(names).forEach(n -> System.out.println(n));
-            names[100] = "Jan"; //ArrayIndexOutOfBoundsException
-            System.out.println("\nTablica liczb");
-            Arrays.stream(numbers).forEach(n -> System.out.println(n));
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println(e.getMessage());
+            int[] numbers = {1, 2, 3, 4};
+            System.out.println("Tablica nazw:");
+            Arrays.stream(names)                      // zamiana tablicy na stream
+                    .forEach(s -> System.out.print(s + ", ")); // dla każdego elementu wykonaj predykat (CTRL + Space)
+            System.out.println("\nTablica liczb:");
+            Arrays.stream(numbers).forEach(n -> System.out.print(n + ", "));
+            System.out.println("Modyfikacja wartości na indeksie");
+            names[5] = "Anna";
+            names[names.length - 1] = "Adam";
+//            names[100] = "Jan";         // może zgłaszać - > ArrayIndexOutOfBoundsException
+            Arrays.stream(names).forEach(s -> System.out.print(s + ", "));
+        } catch (ArrayIndexOutOfBoundsException e) {     // obsługa wyjątku włączana gdy wyjątek danego typu wystąpi
+            System.out.println("Błąd indeksowania tablicy");
         }
+        System.out.println("\nPoza try-catch");
     }
 
     public void listOperations() {
-        //utworzenie pustej listy
+        // utworzenie pustej listy
         List<String> names = new ArrayList<>();
-        //utworzenie listy zainicjowanej wartościami
+        // utowrzenie listy zainicjowanej wartościami
         List<Double> params = new ArrayList<>(Arrays.asList(1.2, 1.44, 1.11, 4.));
         System.out.println(names);
         System.out.println(params);
@@ -40,11 +39,41 @@ public class JavaCollectionsExample {
         return names.contains(name);
     }
 
+    // tablica widoczna w całej klasie
+    List<String> names = new ArrayList<>(Arrays.asList("Jan", "Ala", "Ola", "Ela", "Ala"));
+
+    public void removeElement(String name) {
+        System.out.println(names);
+        names.remove(name);     // usunięcie obiektu o wartości name podanej w argumencie metody
+        System.out.println(names);
+    }
+
+    public void removeAllTheSameElements(String name) {
+        System.out.println(names);
+        names.removeAll(Arrays.asList(name));     // usunięcie obiektu o wartości name podanej w argumencie metody
+        System.out.println(names);
+    }
+
+    // zmień piewrszy i ostatni element na Adam w liście names
+    public void updateFirstAndLastElement(String name) {
+        names.set(0, name);
+        names.set(names.size() - 1, name);
+
+    }
+
+
+    // wypisz wszystkie elementy znajdujące się na parzystych indeksach listy names
+    public void getAllElementsWithOddIndex() {
+        // ???
+    }
+
     public static void main(String[] args) {
         JavaCollectionsExample ex = new JavaCollectionsExample();
-        //   ex.arrayOperations();
-        ex.listOperations();
-        System.out.println(
-                ex.findElement(new ArrayList<>(Arrays.asList("Jan", "Ala", "Ola", "Ela", "Ala")), "Ala"));
+//        ex.arrayOperations();
+//        ex.listOperations();
+//        System.out.println("Wynik: " +
+//                ex.findElement(new ArrayList<>(Arrays.asList("Jan","Ala","Ola","Ela","Ala")), "Ala"));
+        ex.removeAllTheSameElements("Ala");
+        ex.updateFirstAndLastElement("X");
     }
 }

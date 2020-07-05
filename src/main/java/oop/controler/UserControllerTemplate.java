87e487613be53dc994955089a5119a95a86204ea
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-05 10:05:39 +0200

    ``

--- src/main/java/oop/controler/UserControllerTemplate.java
+++ src/main/java/oop/controler/UserControllerTemplate.java
@@ -1,6 +1,9 @@
 package oop.controler;
 
 import oop.model.User;
+import oop.model.enums.Role;
+
+import java.util.Set;
 
 // Interfejs -> szablon wymagań dla klasy go implementującej
 public interface UserControllerTemplate {
@@ -14,5 +17,10 @@ public interface UserControllerTemplate {
     User findUserById(int userId);
     // update password
     void updateUserPassword(int userId, String newPassword);
-    //
+    // delete user
+    void deleteUserById(int userId);
+    // add / drop roles
+    void updateRole(int userId, Set<Role>newRoles);
+    // wypisanie wszstkich użytkowników
+
 }

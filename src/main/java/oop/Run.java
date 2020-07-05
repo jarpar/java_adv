74a54d1091612eb0a76746ef9691a04e7c4d458b
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-05 12:03:48 +0200

    ``

--- src/main/java/oop/Run.java
+++ src/main/java/oop/Run.java
@@ -38,22 +38,27 @@ public class Run {
                 String lastName = scanner.nextLine();
                 System.out.println("Podaj email:");
                 String email = scanner.nextLine();
+                String emailPattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+                if (!Pattern.matches(emailPattern, email)) {
+                    System.out.println("Błędny adres e-mail!");
+                    continue;
+                }
                 System.out.println("Podaj hasło");
                 String password = scanner.nextLine();
                 System.out.println("Podaj płeć (M/K):");
+                String genderPattern = "^[MK]{1}$";
                 String genderInput = scanner.nextLine().toUpperCase();
+                if (!Pattern.matches(genderPattern, genderInput)) {
+                    System.out.println("Błędnie wprowadzona płeć!");
+                    continue;
+                }
                 Gender gender = genderInput.equals("M") ? Gender.MAN : Gender.WOMAN;
                 System.out.println("Podaj telefon (000-000-000):");
                 String phone = scanner.nextLine();
                 //walidacja na podstawie regex-ów
                 String phonePattern = "^[0-9]{3}-[0-9]{3}-[0-9]{3}$";
-                String genderPattern = "^[MK]{1}$";
                 if (!Pattern.matches(phonePattern, phone)) {
                     System.out.println("Błędny numer telefonu!");
-                    continue;
-                }
-                if (!Pattern.matches(genderPattern, genderInput)) {
-                    System.out.println("Błędnie wprowadzona płeć!");
                     continue;
                 }
                 uc.registerUser(new User(name, lastName, email, password, phone, gender));

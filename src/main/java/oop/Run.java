a45642f084c8bff0eaa012f6510b703534608bfb
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-05 10:36:09 +0200

    ``

--- src/main/java/oop/Run.java
+++ src/main/java/oop/Run.java
@@ -1,16 +1,20 @@
 package oop;
 
+import oop.controler.UserControllerTemplate;
 import oop.model.User;
 import oop.model.enums.Gender;
 
 public class Run {
     public static void main(String[] args) {
-        User u1 = new User("name1", "lastName1", "email1", "password1", "phone1", Gender.MAN);
-        User u2 = new User("name2", "lastName2", "email2", "password2", "phone2", Gender.WOMAN);
-        User u3 = new User("name3", "lastName3", "email3", "password3", "phone3", Gender.MAN);
-        System.out.println(u1);
-        System.out.println(u2);
-        System.out.println(u3);
+//        User u1 = new User("name1", "lastName1", "email1", "password1", "phone1", Gender.MAN);
+//        User u2 = new User("name2", "lastName2", "email2", "password2", "phone2", Gender.WOMAN);
+//        User u3 = new User("name3", "lastName3", "email3", "password3", "phone3", Gender.MAN);
+//        System.out.println(u1);
+//        System.out.println(u2);
+//        System.out.println(u3);
+        for (User user : UserControllerTemplate.users
+        ) {
+            System.out.println(user);
+        }
     }
-
 }
--- src/main/java/oop/controler/UserController.java
+++ src/main/java/oop/controler/UserController.java
@@ -1,5 +1,51 @@
 package oop.controler;
 
+import oop.controler.enums.UserField;
+import oop.model.User;
+import oop.model.enums.Role;
+
+import java.util.List;
+import java.util.Set;
+
 //Klasa kontrolera - odpowiedzialna za obsługę i implementację logiki biznesowej aplikacji
-public class UserController {
+public class UserController implements UserControllerTemplate{
+    @Override
+    public void registerUser(User user) {
+
+    }
+
+    @Override
+    public boolean loginUser(String email, String password) {
+        return false;
+    }
+
+    @Override
+    public User findUserById(int userId) {
+        return null;
+    }
+
+    @Override
+    public void updateUserPassword(int userId, String newPassword) {
+
+    }
+
+    @Override
+    public void deleteUserById(int userId) {
+
+    }
+
+    @Override
+    public void updateRole(int userId, Set<Role> newRoles) {
+
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return null;
+    }
+
+    @Override
+    public List<User> findAllUsersOrderByArg(UserField userField, boolean asc) {
+        return null;
+    }
 }

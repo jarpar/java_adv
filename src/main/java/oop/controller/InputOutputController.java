c67a5eb7bfe2f8ec69c543d2b92226fe9b3696c3
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-05 14:24:58 +0200

    ``

--- src/main/java/oop/controller/InputOutputController.java
+++ src/main/java/oop/controller/InputOutputController.java
@@ -1,5 +1,6 @@
 package oop.controller;
 
 public abstract class InputOutputController {
-
+    // globalna ścieżka bezpośednia do pliku
+    private String usersFilePath = "/home/user/java_adv/src/main/resources/file/users.csv";
 }

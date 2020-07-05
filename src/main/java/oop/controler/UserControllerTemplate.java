37089d94443bfc9142a8953494a129b54e0c40e4
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-05 10:20:07 +0200

    ``

--- src/main/java/oop/controler/UserControllerTemplate.java
+++ src/main/java/oop/controler/UserControllerTemplate.java
@@ -2,13 +2,25 @@ package oop.controler;
 
 import oop.controler.enums.UserField;
 import oop.model.User;
+import oop.model.enums.Gender;
 import oop.model.enums.Role;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 // Interfejs -> szablon wymagań dla klasy go implementującej
 public interface UserControllerTemplate {
+    // pole statyczne finalne
+    List<User> users = new ArrayList<>(
+            Arrays.asList(
+                    new User("name1", "lastName1", "email1", "password1", "phone1", Gender.MAN),
+                    new User("name2", "lastName2", "email2", "password2", "phone2", Gender.WOMAN),
+                    new User("name3", "lastName3", "email3", "password3", "phone3", Gender.MAN)
+            )
+    );
+
     // metoda abstrakcyjna -> metoda nieposiadająca ciała - implementacji
     // [typ zwracanej wartości / void] [nazwa metody] ( argument-y / beez argumentów);
     //rejestracja

f682353c2212f92156ebb979a307a8c69a38beda
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-11 12:37:00 +0200

    ``

--- src/main/java/polymorphism/MainPolymorphism.java
+++ src/main/java/polymorphism/MainPolymorphism.java
@@ -1,10 +1,22 @@
 package polymorphism;
 
 public class MainPolymorphism {
+    public static void HasMooreRooms(Company c1, Company c2) {
+        if (c1.getRoomsQuantity() > c2.getRoomsQuantity()) {
+            System.out.println("Firma " + c1.getCompanyName() + " posiada więcej pokoi");
+        } else if (c1.getRoomsQuantity() == c2.getRoomsQuantity()) {
+            System.out.println("Firmy posiadają tyle samo pokoi");
+        } else {
+            System.out.println("Firma " + c2.getCompanyName() + " posiada wiecej pokoi");
+        }
+    }
+
     public static void main(String[] args) {
-        Company openSpace1 = new OpenSpace("X", 13, 11, "A", 110);
+        Company openSpace1 = new OpenSpace("X", 913, 11, "A", 110);
         System.out.println(openSpace1);
         Company openSpace2 = new OpenSpace("Y", 123, 21, "AB", 120);
         System.out.println(openSpace2);
+        System.out.println("===========================================================");
+        HasMooreRooms(openSpace1, openSpace2);
     }
 }

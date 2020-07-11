854b62ae4b9f18adac2c6db84af82fa46d864cac
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-11 11:39:07 +0200

    ``

--- src/main/java/polymorphism/Main.java
+++ src/main/java/polymorphism/Main.java
@@ -3,8 +3,16 @@ package polymorphism;
 public class Main {
     public static void main(String[] args) {
         Company company = new Company();
-        company.setCompanyName("Firma x");
+        company.setCompanyName("XCompany");
         company.setRoomsQuantity(100);
-        System.out.println(company.getCompanyName() + " posiada " + company.getRoomsQuantity() + " pomieszczeń.");
+        System.out.println("Firma " + company.getCompanyName() + " posiada " + company.getRoomsQuantity() + " pomieszczeń.");
+        OpenSpace os1 = new OpenSpace(); // dostęp do wszystkich składowych klasy Openspace i Company
+        os1.setCompanyName("YCompany");
+        os1.setRoomsQuantity(10);
+        os1.setOpenSpaceName("Londyn");
+        os1.setOpenSpaceNo(1);
+        os1.setOpenSpaceCapacity(50);
+        System.out.printf("Firma %s (ilość pomieszczeń: %d) Open space: %d %s (pojemność: %d)",
+                os1.getCompanyName(), os1.getRoomsQuantity(), os1.getOpenSpaceNo(), os1.getOpenSpaceName(), os1.getOpenSpaceCapacity());
     }
 }
--- src/main/java/polymorphism/OpenSpace.java
+++ src/main/java/polymorphism/OpenSpace.java
@@ -4,4 +4,28 @@ public class OpenSpace extends Company {
     private int openSpaceNo; // nr openspace-u
     private String openSpaceName; // nazwa openspac-u
     private int openSpaceCapacity; // liczba pracowników openspac-u
+
+    public int getOpenSpaceNo() {
+        return openSpaceNo;
+    }
+
+    public void setOpenSpaceNo(int openSpaceNo) {
+        this.openSpaceNo = openSpaceNo;
+    }
+
+    public String getOpenSpaceName() {
+        return openSpaceName;
+    }
+
+    public void setOpenSpaceName(String openSpaceName) {
+        this.openSpaceName = openSpaceName;
+    }
+
+    public int getOpenSpaceCapacity() {
+        return openSpaceCapacity;
+    }
+
+    public void setOpenSpaceCapacity(int openSpaceCapacity) {
+        this.openSpaceCapacity = openSpaceCapacity;
+    }
 }

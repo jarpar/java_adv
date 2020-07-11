53244259a3d757cd41aedef6b12d0b476abc7d26
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-11 14:42:05 +0200

    ``

--- src/main/java/lambda_stream_optional/SimpleStreamApi.java
+++ src/main/java/lambda_stream_optional/SimpleStreamApi.java
@@ -23,6 +23,30 @@ public class SimpleStreamApi {
         try {
             numbers.stream().forEach(number -> System.out.printf("%." + precision + "f\t", number));
         } catch (Exception e) {
+            System.out.println("Błęcny zakres precyzji");
+        }
+    }
+
+    public double getSumOfAllNo() {
+        return numbers.stream().mapToDouble(value -> Double.valueOf(value)).sum();
+    }
+
+    public double getMin() {
+        if (numbers.stream().mapToDouble(val -> Double.valueOf(val)).min().isPresent()) {
+            return numbers.stream().mapToDouble(val -> Double.valueOf(val)).min().getAsDouble();
+        } else {
+            System.out.println("Błąd");
+            return 0.0;
+        }
+    }
+
+
+    public double getMax() {
+        if (numbers.stream().mapToDouble(val -> Double.valueOf(val)).max().isPresent()) {
+            return numbers.stream().mapToDouble(val -> Double.valueOf(val)).max().getAsDouble();
+        } else {
+            System.out.println("Błąd");
+            return 0.0;
         }
     }
 
@@ -31,5 +55,9 @@ public class SimpleStreamApi {
         ssa.getAllNumbers();
         ssa.getSeparatedNumbers(";");
         ssa.getShortNumers(2);
+        System.out.println(ssa.getSumOfAllNo());
+        System.out.println(ssa.getMax());
+        System.out.println(ssa.getMin());
+        ;
     }
 }

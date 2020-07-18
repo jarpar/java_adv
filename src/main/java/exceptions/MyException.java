51b72e36467f2af647f56b94f5fb288f18066ed2
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-18 12:23:01 +0200

    ``

---
+++ src/main/java/exceptions/MyException.java
@@ -0,0 +1,21 @@
+package exceptions;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MyException extends Exception {
+    public MyException() {
+        System.out.println("Jestem w konstruktorze MyException");
+
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+        System.out.println("Moja obsługa wyjątku");
+    }
+
+    public String getExceptionDateTime() {
+        return DateTimeFormatter.ofPattern("dd.MM.yyyy'r' '(' HH:mm:ss')'").format(LocalDateTime.now());
+    }
+}

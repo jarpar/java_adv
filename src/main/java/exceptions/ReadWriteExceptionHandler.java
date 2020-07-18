d7e939c8999502835fbc19660bb06d985e72c680
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-18 11:32:45 +0200

    ``

---
+++ src/main/java/exceptions/ReadWriteExceptionHandler.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReadWriteExceptionHandler {
+
+    private static String path = Paths.get("").toAbsolutePath().toString() + "src/main/java/exceptions/data.txt";
+    private File file = new File(path);
+
+    public void appendDataToFile() {
+
+    }
+
+    public void readDAtaFromFile() {
+
+    }
+}

af1a77bc67b24be6cfb32c5da4ec8bd53ad43514
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-18 13:46:09 +0200

    ``

--- src/main/java/threads/ThreadController.java
+++ src/main/java/threads/ThreadController.java
@@ -1,37 +1,40 @@
 package threads;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ThreadController {
+    Deque<Integer> numbers = new ArrayDeque<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-    List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-
+    // metoda implementująca wątek wypisujący wszystkie liczby
     public void printNumbers() {
-        //utworzenie klasy anonimowej
+        // utworzenie klasy anonimowej
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (Integer number : numbers
-                ) {
+                while (!numbers.isEmpty()) {
                     try {
-                        Thread.currentThread().sleep(1000);
-                        System.out.println(Thread.currentThread().getName());
-                        System.out.println(number);
+                        Thread.currentThread().sleep(new Random().nextInt(6) * 1000);     // uśpienie wąktu thread na 1s
+                        System.out.println("Wątek: " + Thread.currentThread().getName() +
+                                " - wartość: " + numbers.removeFirst() +
+                                " aktualna zawartość: " + numbers);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                    } catch (NoSuchElementException e) {
+                        break;
                     }
                 }
             }
         });
-        thread.start();//uruchomienie wątku, czyli wykonanie metody run
+        thread.start();         // uruchomienie wątku -> "wykonanie metody run()"
     }
 
     public static void main(String[] args) {
-        ThreadController threadController = new ThreadController();
-        System.out.println(Thread.currentThread().getName());
-        threadController.printNumbers();
-        System.out.println(Thread.currentThread().getName());
+        ThreadController tc = new ThreadController();
+        System.out.println("Wątek: " + Thread.currentThread().getName());
+        tc.printNumbers();
+        tc.printNumbers();
+        tc.printNumbers();
+        System.out.println("Wątek: " + Thread.currentThread().getName());
+
     }
 }

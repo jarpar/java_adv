d68a12e3528997085c2bd76c044c45f99fabf7e7
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-07-19 10:32:48 +0200

    ``

--- src/main/java/threadsClock/Clock.java
+++ src/main/java/threadsClock/Clock.java
@@ -1,27 +1,36 @@
 package threadsClock;
 
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-//Napisz program wypisujący nq konsoli aktualny czas i odświeżający go co sekundęq
-public class Clock implements Runnable {
-
-    public void printTime() throws InterruptedException {
-        while (true) {
-            System.out.println(DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
-            Thread.sleep(1000);
-
-        }
+public class Clock {
+    // ZEGAREK CYFROWY HH:mm:ss
+    // napisz program wypisujący na konoli aktualny czas i odświeżający go co sekundę
+    private static String editString(int stringLength) {
+        StringBuilder builder = new StringBuilder();        // string edytowalny
+        for (int i = 0; i < stringLength; i++)
+            builder.append("\b");
+        return builder.toString();
+    }
+    public void printTime() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+//                       String currentTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+                        String currentTime = DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalTime.now());
+                        System.out.printf(currentTime);
+                        Thread.currentThread().sleep(1000);
+                        System.out.printf("%s", editString(currentTime.length()));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } } }
+        });
+        thread.start();
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        Clock clock = new Clock();
-        clock.printTime();
-        //Thread thread = new Thread(new Clock());
-    }
-
-    @Override
-    public void run() {
-
+    public static void main(String[] args) {
+        new Clock().printTime();
     }
 }
